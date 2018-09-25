a5b36639164cb455e74d91e4a1c78caf1662644c
sigmaidetest <cehib@balanc3r.com>
2018-09-25 16:17:43 +0530

    Re added user-api table

--- DemoSwaggerDataEndpoint/addUserAPI.js
+++ DemoSwaggerDataEndpoint/addUserAPI.js
@@ -7,19 +7,20 @@ exports.handler = function (event, context, callback) {
     let apiName = event.apiName;
 
     console.log("Subscribing user for API", username, apiName);
-    // ddb.put({
-    //     TableName: 'UserSwaggerSubscriptions',
-    //     Item: { 'username': username, 'apiName': apiName }
-    // }).promise()
-    //     .then((data) => {
-    //         console.log("Successfully added entry", data);
-    //         callback(null, {
-    //             username: username,
-    //             apiName: apiName
-    //         });
 
-    //     }).catch((err) => {
-    //         console.log("Failed to add entry", err);
-    //         callback(err);
-    //     });
+    ddb.put({
+        TableName: 'UserSwaggerSubscriptions',
+        Item: { 'username': username, 'apiName': apiName }
+    }).promise()
+        .then((data) => {
+            console.log("Successfully added entry", data);
+            callback(null, {
+                username: username,
+                apiName: apiName
+            });
+
+        }).catch((err) => {
+            console.log("Failed to add entry", err);
+            callback(err);
+        });
 }
