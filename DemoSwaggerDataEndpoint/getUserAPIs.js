031c1f51dbb45ef1fe7a7ce77427fa17cd33da8d
sigmaidetest <cehib@balanc3r.com>
2018-09-25 17:45:59 +0530

    ws

--- DemoSwaggerDataEndpoint/getUserAPIs.js
+++ DemoSwaggerDataEndpoint/getUserAPIs.js
@@ -1,14 +1,34 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
 
-    console.log(event);
-    callback(null, {
-        "isBase64Encoded": true,
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*"
+    let username = event['queryStringParameters']['username'];
+    console.log("Username", username);
+
+    ddb.query({
+        TableName: 'UserSwaggerSubscriptions',
+        ExpressionAttributeValues: {
+            ':username': username
         },
-        "body": JSON.stringify([]);
-    });
+        KeyConditionExpression: ' S :'
+    }).promise()
+        .then((data) => {
+            console.log("Retrived data", data);
+            callback(null, {
+                "isBase64Encoded": true,
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*"
+                },
+                "body": JSON.stringify([])
+            });
+
+        }).catch((err) => {
+            console.log("Error occurred while retrived data", err);
+            callback(err);
+        });
+
+
+
 }
