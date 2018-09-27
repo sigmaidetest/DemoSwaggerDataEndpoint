1be797393e0937345dd2045e475f89a195645dd8
sigmaidetest <cehib@balanc3r.com>
2018-09-27 10:12:47 +0530

    Added get API versions op

--- DemoSwaggerDataEndpoint/getAPIVersions.js
+++ DemoSwaggerDataEndpoint/getAPIVersions.js
@@ -3,35 +3,33 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
 
-    let username = event['queryStringParameters']['username'];
-    console.log("Username", username);
+    let apiName = event['queryStringParameters']['apiName'];
+    console.log("apiName", apiName);
 
     ddb.query({
-        TableName: 'UserSwaggerSubscriptions',
-        ExpressionAttributeValues: {
-            ':userName': username
-        },
-        KeyConditionExpression: 'username = :userName'
+        TableName: 'SwaggerDetails',
+        ExpressionAttributeValues: { ':apiName': apiName },
+        KeyConditionExpression: 'apiName = :apiName'
     }).promise()
         .then((data) => {
             let items = data.Items;
             console.log("Retrieved items", items);
 
-            let userAPIs = items.map(item => item['apiName']);
+            let apiVersions = items.map(item => {
+                return { "apiVersion": item['apiVersion'], "definition": JSON.parse(item['definition']) };
+            });
+
             callback(null, {
                 "isBase64Encoded": true,
                 "statusCode": 200,
                 "headers": {
                     "Access-Control-Allow-Origin": "*"
                 },
-                "body": JSON.stringify(userAPIs)
+                "body": JSON.stringify(apiVersions)
             });
 
         }).catch((err) => {
             console.log("Error occurred while retriving data", err);
             callback(err);
         });
-
-
-
 }
